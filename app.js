a42d1948102048116eb98d427e9101dbc28a7038
justintemps <jesmith627@gmail.com>
2017-10-06 01:03:16 +0200

    Making progress

--- app.js
+++ app.js
@@ -1,14 +1,53 @@
 const Twitter = require('twitter-node-client').Twitter;
 const config = require('./data/twitter_config.json');
 
-//Callback functions
-var error = function(err, response, body) {
-  console.log('ERROR [%s]', err);
-};
-var success = function(data) {
-  console.log('Data [%s]', data);
-};
-
 const twitter = new Twitter(config);
+const listname = 'List with members';
 
-twitter.getUserTimeline({ screen_name: 'ilo', count: '10'}, error, success);
+// Create a new list
+function createList() {
+  let err, success;
+  return new Promise(function(resolve, reject) {
+    err = (err, response) => {
+      reject(new Error(`Failed to create list: ${err}`));
+    };
+
+    // return the list id if the request is successful
+    success = data => {
+      let {id: listid, slug : slug} = JSON.parse(data);
+      console.log(`${listname} was successfully added`);
+      console.log(`List ID is ${listid}`);
+      console.log(`The slug is ${slug}`);
+      resolve({id: listid, slug: slug, name: listname});
+    };
+
+    // create the list
+    twitter.postList({ name: listname }, err, success);
+  });
+}
+
+// Populate the list with new names
+function populateList(list) {
+  console.log(list);
+  let err = (err, response) => {
+    console.log(err);
+  };
+
+  let success = data => {
+    console.log(JSON.stringify(JSON.parse(data),null,2));
+  };
+
+  twitter.postListMembers(
+    { name: list.name, list_id: list.id, slug: list.slug, user_id: ['112541479'] },
+    err,
+    success
+  );
+}
+
+createList()
+  .then(list => {
+    populateList(list);
+  })
+  .catch(error => {
+    console.log(error);
+  });
