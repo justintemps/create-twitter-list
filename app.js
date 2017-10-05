af4feddd8c747f329b69df5ebf01eec0386a706c
justintemps <jesmith672@gmail.com>
2017-10-05 18:29:11 +0200

    update

--- app.js
+++ app.js
@@ -1,62 +1,14 @@
-const TwitterPinAuth = require('twitter-pin-auth');
-const prompt = require('prompt-promise');
-const axios = require('axios');
+const Twitter = require('twitter-node-client').Twitter;
+const config = require('./data/twitter_config.json');
 
-const CREATE_LIST = 'https://api.twitter.com/1.1/lists/create.json';
-
-// Get tokens from Twitter
-const twitterPinAuth = new TwitterPinAuth(
-  'GSLiSnuT2V3oR5siiKI01baJ5',
-  '8OXRV1qIlfzoReFAocRRE1T7SnxCDB6d6CfnLhIYtdewjU8u88',
-  false
-);
-
-// Authenticate with Twitter
-twitterPinAuth
-  .requestAuthUrl()
-  // Get the auth url from Twitter and print it to the console
-  .then(function(url) {
-    console.log('Navigate to this url in your browser:');
-    console.log(url);
-  })
-  .then(function() {
-    // Prompt the user to enter the pin
-    prompt('Enter PIN: ')
-      .then(function(pin) {
-        prompt.done();
-        return pin;
-      })
-      .then(function(pin) {
-        twitterPinAuth
-          .authorize(pin)
-          .then(function(){
-            console.log('Hurray! You\'re properly logged in now.');
-            createList();
-          })
-          .catch(function(err) {
-            console.error(err);
-          });
-      })
-      .catch(function(err) {
-        console.error(err);
-      });
-  })
-  .catch(function(err) {
-    console.error(err);
-  });
-
-// Create a new list in Twitter
-function createList() {
-  axios
-    .post(CREATE_LIST, {
-      name: 'Brand new list',
-      mode: 'public',
-      description: 'This is a test.'
-    })
-    .then(function(response) {
-      console.log(response);
-    })
-    .catch(function(error) {
-      console.log(error);
-    });
+//Callback functions
+var error = function(err, response, body) {
+  console.log('ERROR [%s]', err);
 };
+var success = function(data) {
+  console.log('Data [%s]', data);
+};
+
+const twitter = new Twitter(config);
+
+twitter.getUserTimeline({ screen_name: 'ilo', count: '10'}, error, success);
